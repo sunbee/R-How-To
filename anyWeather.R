dfda683685dece5b83fb966248b0d4045a3af194
Sanjay Bhatikar <sanjay.bhatikar@gmail.com>
2015-06-29 23:04:01 +0530

    Added feature to handle multiple locations and timestamps with data.table

--- anyWeather.R
+++ anyWeather.R
@@ -2,15 +2,26 @@
 # Get the weather at any time, anywhere
 
 library(plyr)
+library(data.table)
 
 fetchLocations <- function(locations) {
   read.csv(locations)
 }
 
 fetchTimeStamps <- function(timeStamps) {
-  read.csv(timeStamps)
+  read.csv(timeStamps, stringsAsFactors=FALSE)
 }
 
+base <- "C:/Users/ssbhat3/Desktop/R-HOW-TO/"
+locations <- "locations.csv"
+locations <- paste0(base, locations)
+timestamps <- "timestamps.csv"
+timestamps <- paste0(base, timestamps)
+
+loc <- fetchLocations(locations)
+tim <- fetchTimeStamps(timestamps)
+
+locTim <- merge(loc, tim)
 
 getWeather <- function(lat, long, timeStamp) {
   baseurl <- "https://api.forecast.io/forecast";
@@ -19,7 +30,10 @@ getWeather <- function(lat, long, timeStamp) {
   lat.long.time <- paste(lat, long, timeStamp, sep=",")
   sourceURL <- paste(baseurl, weather_key, lat.long.time, sep="/")
   
-  out <- fromJSON(sourceURL)
+  sourceURL
+  w <- fromJSON(sourceURL)
+  w
 }
 
 w <- getWeather(lat="12.9833", long="77.5833", timeStamp="2015-06-29T12:00:00+0530")
+
